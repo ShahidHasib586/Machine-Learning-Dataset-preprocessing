471d7adef58ddc7178148f09016190a719942010
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-05-09 09:25:56 +0600

    Add files via upload

---
+++ data pre processiing templet.R
@@ -0,0 +1,8 @@
+#Eclat model
+
+#data preprocessing
+
+#install packages
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
